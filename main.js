2721b4e2189968384cfb6cb235831272611e10f6
John (Jef) Fisher <Fishe545@gmail.com>
2017-05-10 11:09:33 -0700

    files initialized and skeleton somewhat made

---
+++ main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Katsurio on 5/10/17.
+ */
+/** @function - Function that shuffles the cards' images.
+ * @name shuffleCards
+ * @param {String} cardBackImg - A strings that contains the path to the cards' back image.
+ * @param {String[]} cardFaceImgs - An array of strings that contain the paths to the card faces' images.
+ */
